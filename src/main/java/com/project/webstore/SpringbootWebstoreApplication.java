25fe8de7c099fcd70bcb191ccdaba95788841d22
Edder Luan <eluann100@gmail.com>
2021-08-12 14:27:55 -0300

    first commit

---
+++ src/main/java/com/project/webstore/SpringbootWebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebstoreApplication.class, args);
+	}
+
+}
