380cf99a5914a1da81c262386fa1acf982852040
Edder Luan <eluann100@gmail.com>
2021-08-23 15:48:35 -0300

    Operações de instanciação

--- src/main/java/com/project/webstore/SpringbootWebstoreApplication.java
+++ src/main/java/com/project/webstore/SpringbootWebstoreApplication.java
@@ -1,13 +1,31 @@
 package com.project.webstore;
 
+import com.project.webstore.domains.Categories;
+import com.project.webstore.repositories.CategoriesRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebstoreApplication {
+public class SpringbootWebstoreApplication implements CommandLineRunner {
+    
+    @Autowired
+    private CategoriesRepository categoriesRepository;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebstoreApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebstoreApplication.class, args);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categories cat1 = new Categories(null, "Informática");
+        Categories cat2 = new Categories(null, "Escrtitório");
+
+        categoriesRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 
 }
