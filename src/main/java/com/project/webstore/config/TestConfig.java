cba3a49b87b39b5c047f718e10faabdddf30a3b9
Edder Luan <eluann100@gmail.com>
2021-09-24 23:01:13 -0300

    Injeção de dados via dbservice

---
+++ src/main/java/com/project/webstore/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.project.webstore.config;
+
+import com.project.webstore.services.DBService;
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    
+    @Autowired
+    private DBService dbservice;
+    
+    @Bean
+    public boolean instantiateDataBase() throws ParseException{
+        dbservice.instantiateTestDatabase();
+        return true;
+    }
+    
+}
